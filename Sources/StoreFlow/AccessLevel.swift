711413c13a924f774e80aa956597c99ed0110864
Gerard Gomez <gerardgomez11@me.comgerardgomez11@me.comgerardgomez11@me.com>
2025-05-25 23:16:37 -0400

    [feat] add environment for acess level

--- Sources/StoreFlow/AccessLevel.swift
+++ Sources/StoreFlow/AccessLevel.swift
@@ -6,8 +6,13 @@
 //
 
 import Foundation
+import SwiftUI
 
 public enum AccessLevel: Int, Codable, Comparable, Sendable {
     case notSubscribed, individual, family, premium
     public static func < (lhs: Self, rhs: Self) -> Bool { lhs.rawValue < rhs.rawValue }
+}
+
+public extension EnvironmentValues {
+    @Entry var accessLevel: AccessLevel = .notSubscribed 
 }
